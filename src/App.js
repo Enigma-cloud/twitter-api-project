6e6dbd5e822849363a513bea97804868ad66a66f
unknown <torreskristian77@gmail.com>
2021-10-23 19:44:39 +0100

    fs: tests design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <div className="container mx-auto">
-          <h1 className="flex"></h1>
+        <div className="w-full h-screen flex">
+          <header className="bg-gray-400 h-screen w-24">
+            <a href="/" className="font-semibold">Title</a>
+          </header>
+          <div className="container"></div>
         </div>
     </div>
   );
